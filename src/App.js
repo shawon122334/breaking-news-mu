ae0d1b98b6e97e360e72cf0a3c4f30ae37cbd1c8
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-04-08 02:16:51 +0600

    title image and description added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import News from './Components/News/News';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [articles,setArticles]= useState([]);
+  useEffect(()=>{
+    const url='https://newsapi.org/v2/top-headlines?country=us&apiKey=819940a0ad13485c90a38a9c180ae1ad';
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>setArticles(data.articles))
+  },[])
   return (
     <div>
-      <News></News>
+      {
+        articles.map(article=> <News article={article}></News> )
+      }
     </div>
   );
 }
